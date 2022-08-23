e15778b7a4749a7bff1beeab962b3f2b10583b88
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-08-24 00:45:02 +0500

    commit courrant

---
+++ HomeWork_23_08_22/Ex41_num_up_nul/Program.cs
@@ -0,0 +1,19 @@
+﻿// Ex41 - Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+    // 0, 7, 8, -2, -2 -> 2           1, -7, 567, 89, 223-> 3
+
+void NumbersUpNul(int kolvo)
+{
+    Console.WriteLine();
+    Console.WriteLine("Введите числа через Enter");
+    int count = 0;
+    for (int i = 1; i <= kolvo; i++)
+    {
+        int num = Convert.ToInt32(Console.ReadLine());
+        if (num > 0) count++;
+    }
+    Console.WriteLine($"Количество чисел, больших нуля, равно {count}");
+}
+Console.Write("Задайте количество вводимых чисел, М = ");
+int M = Convert.ToInt32(Console.ReadLine());
+NumbersApNul(M);
