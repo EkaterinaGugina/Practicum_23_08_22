2c14153a96779a89e06d7fbd65e8abb526d7e756
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-08-24 01:38:36 +0500

    perfected Ex39

--- Ex39_revers_number/Program.cs
+++ Ex39_revers_number/Program.cs
@@ -2,23 +2,22 @@
 //[1 2 3 4 5] -> [5 4 3 2 1]
 //[6 7 3 6] -> [6 3 7 6]
  
-void FillArray(int[] array)       //заполнение массива случайными числами
+void FillArray(int[] array)       //заполнение массива случайными числами и вывод на экран
 {
-    int length = array.Length;
-    for (int index = 0; index < length; index++)
-    {
-        array[index] = new Random().Next(-99, 100);
-        Console.WriteLine($"[{string.Join(" ", array)}]");
-    }
+    for (int index = 0; index < array.Length; index++)  array[index] = new Random().Next(-99, 100);
+    Console.WriteLine($"[{string.Join(", ", array)}]");
 }
 
 void ReversArray(int[] arr)             // вывод на печать реверсом данного массива
 {
-    for (int pozition = arr.Length - 1; pozition >= 0; pozition--)
+    Console.Write("[");
+    for (int pozition = arr.Length - 1; pozition > 0; pozition--)
     {
-        Console.Write($" {arr[pozition]}");
+        Console.Write($"{arr[pozition]}, ");
     }
+    Console.Write($"{arr[0]}]");
 }
+
 Console.Write("Введите размер массива, size = ");  //заполнение и печать массива
 int size = Convert.ToInt32(Console.ReadLine());
 int[] array39 = new int[size];
